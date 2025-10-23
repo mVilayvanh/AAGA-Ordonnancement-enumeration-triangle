7821719bc171318a51c5e8a2592cb5a1dfb7efb3
mVilayvanh <mickaelvilayvanh@gmail.com>
2025-10-23 14:56:08 +0200

    add: a++ et a+- fait

--- triangle-algorithms.py
+++ triangle-algorithms.py
@@ -1,6 +1,6 @@
 import utils.directedGraph as dg
 
-def triangle_listing(g:dg.DirectedGraph):
+def A_plus_plus(g:dg.DirectedGraph):
     b = {u: False for u in g.nodes}
     # ici il faudra modifier g.nodes pour changer l'ordre de parcours
     for w in g.nodes:
@@ -12,4 +12,16 @@ def triangle_listing(g:dg.DirectedGraph):
                     print(f"Triangle found: ({u}, {v}, {w})")
         for v in g.predecessors(w):
             b[v] = False
+
+def A_plus_minus(g:dg.DirectedGraph):
+    b = {u: False for u in g.nodes}
+    for u in g.nodes:
+        for w in g.successors(u):
+            b[w] = True
+        for v in g.successors(u):
+            for w in g.successors(v):
+                if b[w]:
+                    print(f"Triangle found: ({u}, {v}, {w})")
+        for w in g.successors(u):
+            b[w] = False
 
