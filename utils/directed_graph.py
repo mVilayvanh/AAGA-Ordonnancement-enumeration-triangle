139f4503fc1fa4fd6064cc69a3a010354350c8b4
mVilayvanh <mickaelvilayvanh@gmail.com>
2025-10-24 11:04:47 +0200

    update: rank_node_by_degree function to correctly compare

--- utils/directed_graph.py
+++ utils/directed_graph.py
@@ -5,17 +5,17 @@
 # Tailored vertex ordering for faster triangle listing in large graphs
 
 class DirectedGraph:
-    def __init__(self):
+    def __init__(self) -> None:
         self.edges = dict()
         self.nodes = set()
         self.in_degrees = {node: 0 for node in self.nodes}
         self.out_degrees = {node: 0 for node in self.nodes}
         self.computed_degrees = False
         
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.adj)
 
-    def add_edge(self, u, v):
+    def add_edge(self, u, v) -> None:
         if u not in self.edges:
             self.edges[u] = []
         self.edges[u].append(v)
@@ -23,26 +23,26 @@ class DirectedGraph:
         self.nodes.add(v)
 
 
-    def get_neighbors(self, node):
+    def get_neighbors(self, node) -> list:
         return self.edges.get(node, [])
 
-    def is_successor(self, u, v):
+    def is_successor(self, u, v) -> bool:
         return v in self.adj.get(u, [])
     
-    def is_predecessor(self, u, v):
+    def is_predecessor(self, u, v) -> bool:
         return u in self.adj.get(v, [])
 
-    def successors(self, node):
+    def successors(self, node) -> list:
         return self.edges.get(node, [])
     
-    def predecessors(self, node):
+    def predecessors(self, node) -> list:
         preds = []
         for u in self.edges:
             if node in self.edges[u]:
                 preds.append(u)
         return preds
 
-    def compute_degrees(self):
+    def compute_degrees(self) -> None:
         if self.computed_degrees:
             return
         for u in self.edges:
@@ -51,17 +51,17 @@ class DirectedGraph:
                 self.in_degrees[v] += 1
         self.computed_degrees = True
     
-    def get_in_degree(self, node):
+    def get_in_degree(self, node) -> int:
         if not self.computed_degrees:
             self.compute_degrees()
         return self.in_degrees.get(node, 0)
     
-    def get_out_degree(self, node):
+    def get_out_degree(self, node) -> int:
         if not self.computed_degrees:
             self.compute_degrees()
         return self.out_degrees.get(node, 0)
     
-    def get_degree(self, node):
+    def get_degree(self, node) -> int:
         if not self.computed_degrees:
             self.compute_degrees()
         return self.get_in_degree(node) + self.get_out_degree(node)
