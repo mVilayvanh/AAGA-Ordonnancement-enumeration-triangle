48ca7125affc8fe3f762e18dadb50bd8dff0f2a5
Mickael VILAYVANH <mickaelvilayvanh@gmail.com>
2025-11-03 19:42:31 +0100

    update: plotbuilder defined

--- utils/plotbuilder.py
+++ utils/plotbuilder.py
@@ -2,37 +2,64 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class PlotBuilder:
-    def __init__(self, size=(10, 6), ndataset=1):
-        self.fig, self.ax = plt.subplots(2, 2, figsize=size)
-        self.x = np.arange(ndataset)
-        self.width = 1 / ndataset
-        self.ndataset = ndataset
+    def __init__(self):
+        self.data = {}
 
-    def set_title(self, title, subplot=(0, 0)):
-        i, j = subplot
-        self.ax[i][j].set_title(title)
+    def add_dataset(self, dataset: str):
+        """Ajoute un dataset à la structure de données."""
+        if dataset not in self.data:
+            self.data[dataset] = {}
 
-    def plot_clustered_bar(self, data, xlabel, subplot=(0, 0)):
-        i, j = subplot
-        ax = self.ax[i][j]
-        ax.bar([self.x - self.width], data[0], width=self.width)
-        ax.set_xticks(self.x)
-        ax.set_xticklabels(xlabel)
+    def add_order(self, dataset: str, order: str):
+        """Ajoute un ordonnancement pour un dataset donné."""
+        if dataset not in self.data:
+            self.add_dataset(dataset)
+        if order not in self.data[dataset]:
+            self.data[dataset][order] = None
 
-    def show(self):
+    def add_time(self, dataset: str, order: str, time: float):
+        """Ajoute un temps d'exécution pour un dataset et un ordonnancement donnés."""
+        if dataset not in self.data:
+            self.add_dataset(dataset)
+        if order not in self.data[dataset]:
+            self.add_order(dataset, order)
+        self.data[dataset][order] = time
+
+    def show(self, title="Temps d'exécution par dataset et ordonnancement", ylabel="Temps (s)"):
+        """Affiche un graphique en barres des temps d'exécution."""
+        if not self.data:
+            return
+        datasets = list(self.data.keys())
+        ordonnancements = sorted({o for d in self.data.values() for o in d.keys()})
+        values = np.array([
+            [self.data[d].get(o, 0) or 0 for o in ordonnancements] for d in datasets
+        ])
+        x = np.arange(len(datasets))
+        width = 0.8 / len(ordonnancements)
+        plt.figure(figsize=(10, 6))
+        for i, o in enumerate(ordonnancements):
+            plt.bar(
+                x + i * width,
+                values[:, i],
+                width,
+                label=o)
+        plt.xticks(x + width * (len(ordonnancements) - 1) / 2, datasets)
+        plt.ylabel(ylabel)
+        plt.title(title)
+        plt.legend(title="Ordonnancement")
+        plt.grid(axis='y', linestyle='--', alpha=0.7)
         plt.tight_layout()
         plt.show()
 
+    def __repr__(self):
+        return f"PlotBuilder(data={self.data})"
 
 if __name__ == "__main__":
-    # pb = PlotBuilder(nmethods=5)
-    # pb.set_title("Test Plot", subplot=(0, 0))
-    # pb.plot_clustered_bar([[1, 2, 3, 6, 7], [2, 3, 4, 1, 0], [3, 4, 5, 2, 2]], xlabel=["A"], subplot=(0, 0))
-    # pb.show()
-    # [[Donnée1] [Donnée2] ...]
-    # [[valeur 1= methode1, valeur 2= méthode2, ...], [valeur 1= méthode1, valeur 2= méthode2, ...], ...]
-    data = [[4, 5, 6, 5], [5, 1, 3, 6]]
-    for i in range(len(data)):
-        plt.bar(np.arange(len(data[i])) + i * 0.2, data[i], width=0.2)
-    plt.xticks(np.arange(len(data[0])) + 0.2, ['A', 'B', 'C', 'D'])
-    plt.show()
+    pb = PlotBuilder()
+    pb.add_time("Dataset1", "OrderA", 2.5)
+    pb.add_time("Dataset1", "OrderB", 3.0)
+    pb.add_time("Dataset2", "OrderA", 1.5)
+    pb.add_time("Dataset2", "OrderB", 2.0)
+    pb.add_time("Dataset3", "OrderA", 4.0)
+    pb.add_time("Dataset3", "OrderB", 3.5)
+    pb.show()
